8d865937306fa58197e6a15135c6ff5eb1130c18
DeveloperAmrit <105799602+DeveloperAmrit@users.noreply.github.com>
2025-05-15 17:21:30 +0530

    modified:   src/pages/Coding.jsx

--- src/pages/Coding.jsx
+++ src/pages/Coding.jsx
@@ -4,6 +4,7 @@ import MachineLearning from '../components/coding/MachineLearning'
 import WebDevlopment from '../components/coding/WebDevlopment'
 
 const Coding = () => {
+  window.scrollTo(0,0)
   return (
     <div className='bg-gradient-to-b from-[#0a192f] to-[#0a192f] text-white divide-y divide-gray-800'>
       <WebDevlopment/>
